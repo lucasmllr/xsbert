86aaaea2a6c89fbc7379c71af7f72bfb4af8d94f
lucasmllr <lucasmoeller@me.com>
2023-10-31 12:49:56 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='xsbert',
+    version='0.1.0',    
+    description='explainable sentence transformers',
+    author='Lucas Moeller',
+    author_email='lucasmoeller@me.com',
+    license='BSD 2-clause',
+    packages=['xsbert'],
+    install_requires=[
+        'sentence-transformers',
+        'matplotlib'
+        ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
