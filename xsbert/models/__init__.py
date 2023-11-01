db96c047b8adc9d50dc77daf0c8fdccc4122b309
lucasmllr <lucasmoeller@me.com>
2023-11-01 16:36:07 +0100

    importing load_checkpoint method

--- xsbert/models/__init__.py
+++ xsbert/models/__init__.py
@@ -2,3 +2,4 @@ from .ReferenceTransformer import ReferenceTransformer
 from .ShiftingReferenceTransformer import ShiftingReferenceTransformer
 from .XSTransformer import XSTransformer, XSMPNet, XSRoberta
 from .dot_loss import DotSimilarityLoss
+from .utils import load_checkpoint
