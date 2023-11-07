2ae83a5a9c2de4e762c35cad5cb140aadc9a7952
lucasmllr <lucasmoeller@me.com>
2023-11-07 17:02:30 +0100

    util to download model from ims webserver

--- xsbert/models/__init__.py
+++ xsbert/models/__init__.py
@@ -2,4 +2,4 @@ from .ReferenceTransformer import ReferenceTransformer
 from .ShiftingReferenceTransformer import ShiftingReferenceTransformer
 from .XSTransformer import XSTransformer, XSMPNet, XSRoberta
 from .dot_loss import DotSimilarityLoss
-from .utils import load_checkpoint
+from .utils import load_checkpoint, load_model
--- xsbert/models/utils.py
+++ xsbert/models/utils.py
@@ -1,6 +1,9 @@
 import zipfile
 import os
 from os.path import join, exists
+from os import PathLike
+import wget
+import sys
 
 from . import XSMPNet, XSRoberta
 
@@ -23,4 +26,32 @@ def load_checkpoint(model_name: str):
     elif model_name.endswith('mpnet'):
         model = XSMPNet(path)
 
+    return model
+
+
+def progress(current, total, width):
+  progress_message = f"downloading model: {int(current / total * 100)}%"
+  sys.stdout.write("\r" + progress_message)
+  sys.stdout.flush()
+
+def load_model(name: str, model_dir: PathLike = '../xs_models/'):
+    assert name in ['xs_mpnet', 'xs_distilroberta'], \
+        'available models are: xs_mpnet and xs_distilroberta'
+    if not exists(model_dir):
+        os.makedirs(model_dir)
+    path = join(model_dir, name)
+    if not exists(path):
+        zip_path = path + '.zip'
+        if not exists(zip_path):
+            url = f'https://www2.ims.uni-stuttgart.de/data/xsbert/{name}.zip'
+            wget.download(url, zip_path, bar=progress)
+            print()
+        print('unzipping')
+        with zipfile.ZipFile(zip_path, 'r') as f:
+            f.extractall(model_dir)
+    print('initializing')
+    if name.endswith('roberta'):
+        model = XSRoberta(path)
+    elif name.endswith('mpnet'):
+        model = XSMPNet(path)
     return model
