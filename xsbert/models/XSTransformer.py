3cbcc902af6ae0ef0beef02d7a87aa8d7f743aec
lucasmllr <lucasmoeller@me.com>
2024-03-25 16:49:27 +0100

    updating attributions for cos sim

--- xsbert/models/XSTransformer.py
+++ xsbert/models/XSTransformer.py
@@ -5,7 +5,6 @@ from typing import Tuple
 
 from ..utils import input_to_device
 from . import hooks
-from transformers.models import mpnet
 
 
 class XSTransformer(SentenceTransformer):
@@ -34,13 +33,6 @@ class XSTransformer(SentenceTransformer):
         tokens = ['CLS'] + tokens + ['EOS']
         return tokens
 
-    def _forward_text(self, text):
-        inpt = self[0].tokenize([text])
-        device = self[0].auto_model.embeddings.word_embeddings.weight.device
-        input_to_device(inpt, device)
-        features = self.forward(inpt)
-        return features
-
     def _compute_integrated_jacobian(
             self, 
             embedding: torch.Tensor, 
@@ -63,41 +55,49 @@ class XSTransformer(SentenceTransformer):
         J = torch.stack(jacobi) / self.N_steps
         J = J[:, :-1, :, :].sum(dim=1)
         return J
-    
-    def attribute_prediction(
+        
+    def explain_similarity(
             self, 
             text_a: str, 
             text_b: str, 
-            compute_lhs: bool = True,
+            sim_measure: str = 'cos',
+            return_lhs_terms: bool = False,
             move_to_cpu: bool = False,
             verbose: bool = True,
             compress_embedding_dim: bool = True
             ):
-        
-        self.eval()
-        self.intermediates.clear()  # empty hook cache
 
-        features_a = self._forward_text(text_a)
+        assert sim_measure in ['cos', 'dot'], f'invalid argument for sim_measure: {sim_measure}'
+
+        self.intermediates.clear()
+        device = self[0].auto_model.embeddings.word_embeddings.weight.device
+
+        #TODO: this should be a method
+        inpt_a = self[0].tokenize([text_a])
+        input_to_device(inpt_a, device)
+        features_a = self.forward(inpt_a)
         emb_a = features_a['sentence_embedding']
-        interm_a = self.intermediates[0]  # get intermediate representation from hook cache
+        interm_a = self.intermediates[0]
         J_a = self._compute_integrated_jacobian(emb_a, interm_a, move_to_cpu=move_to_cpu, verbose=verbose)
-        N, Sa, Da = J_a.shape
-        J_a = J_a.reshape((N, Sa * Da))  # flatten feature dimensions
+        D, Sa, Da = J_a.shape
+        J_a = J_a.reshape((D, Sa * Da))
 
-        da = interm_a[0] - interm_a[-1]  # (a - r)
+        da = interm_a[0] - interm_a[-1]
         da = da.reshape(Sa * Da, 1).detach()
 
-        features_b = self._forward_text(text_b)
+        inpt_b = self[0].tokenize([text_b])
+        input_to_device(inpt_b, device)
+        features_b = self.forward(inpt_b)
         emb_b = features_b['sentence_embedding']
         interm_b = self.intermediates[1]
         J_b = self._compute_integrated_jacobian(emb_b, interm_b, move_to_cpu=move_to_cpu, verbose=verbose)
         _, Sb, Db = J_b.shape
-        J_b = J_b.reshape((N, Sb * Db))
+        J_b = J_b.reshape((D, Sb * Db))
 
-        db = interm_b[0] - interm_b[-1]  # (b - r)
+        db = interm_b[0] - interm_b[-1]
         db = db.reshape(Sb * Db, 1).detach()
 
-        J = torch.mm(J_a.T, J_b)  # matrix product of integrated jacobians
+        J = torch.mm(J_a.T, J_b)
         da = da.repeat(1, Sb * Db)
         db = db.repeat(1, Sa * Da)
         if move_to_cpu:
@@ -105,44 +105,53 @@ class XSTransformer(SentenceTransformer):
             db = db.detach().cpu()
             emb_a = emb_a.detach().cpu()
             emb_b = emb_b.detach().cpu()
-        A = da * J * db.T  # element-wise multiplication of feature-pairs
-        A = A.reshape(Sa, Da, Sb, Db)  # reshaping to sequential and embedding dim
+        A = da * J * db.T
+        if sim_measure == 'cos':
+            A = A / torch.norm(emb_a[0]) / torch.norm(emb_b[0])
+        A = A.reshape(Sa, Da, Sb, Db)
         if compress_embedding_dim:
-            A = A.sum(dim=(1, 3))  # token-token attributions
+            A = A.sum(dim=(1, 3))
         A = A.detach().cpu()
 
         tokens_a = self.tokenize_text(text_a)
         tokens_b = self.tokenize_text(text_b)
 
-        if compute_lhs:  # compute the four terms in the ansatz explicitly
+        if return_lhs_terms:
             ref_a = features_a['reference']
             ref_b = features_b['reference']
             if move_to_cpu:
                 ref_a = ref_a.detach().cpu()
                 ref_b = ref_b.detach().cpu()
-            # the first term is the prediction
-            score = torch.dot(emb_a[0], emb_b[0]).item()
-            # the last three terms must be zero
-            ra = torch.dot(emb_a[0], ref_a).item()
-            rb = torch.dot(emb_b[0], ref_b).item()
-            rr = torch.dot(ref_a, ref_b).item()
-            return A, tokens_a, tokens_b, score, ra, rb, rr
+            if sim_measure == 'cos':
+                score = torch.cosine_similarity(emb_a[0].unsqueeze(0), emb_b[0].unsqueeze(0)).item()
+                ref_emb_a = torch.cosine_similarity(emb_a[0].unsqueeze(0), ref_b.unsqueeze(0)).item()
+                ref_emb_b = torch.cosine_similarity(emb_b[0].unsqueeze(0), ref_a.unsqueeze(0)).item()
+                ref_ref = torch.cosine_similarity(ref_a.unsqueeze(0), ref_b.unsqueeze(0)).item()
+            elif sim_measure == 'dot':
+                score = torch.dot(emb_a[0], emb_b[0]).item()
+                ref_emb_a = torch.dot(emb_a[0], ref_b).item()
+                ref_emb_b = torch.dot(emb_b[0], ref_a).item()
+                ref_ref = torch.dot(ref_a, ref_b).item()
+            return A, tokens_a, tokens_b, score, ref_emb_a, ref_emb_b, ref_ref
         else:
             return A, tokens_a, tokens_b
         
 
-    def explain_by_decomposition(self, text_a: str, text_b: str):
-        '''element-wise decomposition of dot-product between output embeddings'''
+    def explain_by_decomposition(self, text_a: str, text_b: str, normalize: bool = False):
         
         device = self[0].auto_model.embeddings.word_embeddings.weight.device
         
         inpt_a = self[0].tokenize([text_a])
         input_to_device(inpt_a, device)
         emb_a = self.forward(inpt_a)['token_embeddings'][0]
+        if normalize:
+            emb_a = emb_a / torch.sum(emb_a)
 
         inpt_b = self[0].tokenize([text_b])
         input_to_device(inpt_b, device)
         emb_b = self.forward(inpt_b)['token_embeddings'][0]
+        if normalize:
+            emb_b = emb_b / torch.sum(emb_b)
 
         A = torch.mm(emb_a, emb_b.t()).detach().cpu()
         A = A / emb_a.shape[0] / emb_b.shape[0]
@@ -187,6 +196,8 @@ class XSRoberta(XSTransformer):
         if hasattr(self, 'hook'):
             self.hook.remove()
             self.hook = None
+        else:
+            print('No hook has been registered.')
 
 
 class XSMPNet(XSTransformer):
@@ -241,6 +252,6 @@ if __name__ == '__main__':
     textb = 'The shown principle generalizes to other areas.'    
 
     explainer.init_attribution_to_layer(idx=11, N_steps=250)
-    A, ta, tb, ab, ra, rb, rr = explainer.explain_similarity(texta, textb, move_to_cpu=True, return_score=True, sim_measure='cos')
+    A, ta, tb, ab, ra, rb, rr = explainer.explain_similarity(texta, textb, move_to_cpu=True, return_lhs_terms=True, sim_measure='cos')
     print(A.sum().item(), ab - ra - rb + rr)
     print(ab, ra, rb, rr)
